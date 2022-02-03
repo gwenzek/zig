81d7ec626c66cf8a053c4911b9fa70fae0ed61db
Andrew Kelley <andrew@ziglang.org>
2022-02-03 16:49:24 -0700

    update C API bindings to LLVM 14
     * zig_clang is fully updated
     * zig_llvm is fully updated
    
    Some initial work on codegen.cpp is in place for upgrading to LLVM's
    new opaque pointers. However there is much more to be done.
    
    A few of zig llvm bindings for deprecated functions have been updated;
    more need to be updated.

--- lib/std/target.zig
+++ lib/std/target.zig
@@ -805,6 +805,8 @@ pub const Target = struct {
             hsail64,
             spir,
             spir64,
+            spirv32,
+            spirv64,
             kalimba,
             shave,
             lanai,
@@ -816,8 +818,6 @@ pub const Target = struct {
             // Stage1 currently assumes that architectures above this comment
             // map one-to-one with the ZigLLVM_ArchType enum.
             spu_2,
-            spirv32,
-            spirv64,
 
             pub fn isX86(arch: Arch) bool {
                 return switch (arch) {
